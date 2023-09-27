01d7712603aef503400c1c9fda0235c23ff97681
Emilio <emilio_casado_de_galdo@hotmail.com>
2023-09-27 17:56:23 +0100

    https

--- src/context/TiempoProvider.jsx
+++ src/context/TiempoProvider.jsx
@@ -25,7 +25,7 @@ const TiempoProvider = ({children}) => {
         try {
             setTiempo({})
             const apiKey = import.meta.env.VITE_API_KEY
-            const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},ES&appid=${apiKey}&lang=sp`
+            const url = `https://api.openweathermap.org/geo/1.0/direct?q=${ciudad},ES&appid=${apiKey}&lang=sp`
             const response = await axios(url)
             const {lat, lon} = response.data[0]
             const urlApi = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&lang=sp`
