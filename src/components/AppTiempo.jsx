57aa31f75f51aceec816b06bafb1e67b85123136
Emilio <emilio_casado_de_galdo@hotmail.com>
2023-09-27 16:56:15 +0100

    responsive fixed

--- src/components/AppTiempo.jsx
+++ src/components/AppTiempo.jsx
@@ -11,7 +11,7 @@ const AppTiempo = () => {
         <header className="mt-4">
             <h1 className="text-center align-middle text-4xl font-extrabold text-white">App Tiempo</h1>
         </header>
-        <main className="bg-white w-full xs:1/1 sm:w-2/3 md:w-2/3 lg:w-2/4 xl:w-2/4 mx-auto mt-4 p-10">
+        <main className="bg-white w-full xs:1/1 sm:w-2/3 md:w-2/3 lg:w-2/4 xl:w-2/4 2xl:w-2/4 mx-auto mt-4 p-10">
             <Formulario/>
             {tiempo?.main && <Tiempo/>}
         </main>
